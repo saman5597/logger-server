6b3708f464d278633f56e24883b9c30f530f0c51
tarun00001 <you@example.com>
2021-10-22 11:25:58 +0530

    filter on logTypes

--- helper/queryHelper.js
+++ helper/queryHelper.js
@@ -6,8 +6,12 @@ class QueryHelper {
 
     filter() {
         const queryObj = { ...this.queryStr };
+        //console.log(queryObj)
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        //console.log(excludedFields)
         excludedFields.forEach(el => delete queryObj[el]);
+        // console.log(queryObj.startDate)
+        //console.log(excludedFields,queryObj)
         if (queryObj.startDate && queryObj.endDate) {
             queryObj.createdAt = { gte: new Date(queryObj["startDate"]), lte: new Date(queryObj["endDate"]) }
         } else if (queryObj.startDate) {
@@ -16,8 +20,11 @@ class QueryHelper {
             queryObj.createdAt = { lte: new Date(queryObj["endDate"]) }
         }
         let queryStr = JSON.stringify(queryObj);
+        //console.log(queryStr)
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        //console.log(queryStr)
         this.query = this.query.find(JSON.parse(queryStr));
+        //console.log(this)
         return this;
     }
 
@@ -37,6 +44,14 @@ class QueryHelper {
         const limit = parseInt(this.queryStr.limit) || 100;
         const skip = (page - 1) * limit;
         this.query = this.query.skip(skip).limit(limit);
+        return this;
+    }
+
+    logFilter() {
+        const queryObj = { ...this.queryStr };
+        console.log(queryObj)
+        var result = (queryObj.logType).split('-');
+        this.query = this.query.find({logType:result});
         return this;
     }
 }
