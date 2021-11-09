507b397a92201ecc57c545bce6792ec28ed581af
Saman Arshad <saman.arshad97@gmail.com>
2021-11-09 11:45:21 +0530

    Added proxy support

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ connectDB();
 
 
 const app = express();
+app.enable('trust proxy')
 app.use(cors())
 app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb",extended:true}))
