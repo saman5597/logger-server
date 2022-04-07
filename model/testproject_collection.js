55d30daed68ff763c73b55fbefeaa5a07d804e99
Saman <saman.arshad97@gmail.com>
2022-04-07 15:06:25 +0530

    Added new collection

---
+++ model/testproject_collection.js
@@ -0,0 +1,38 @@
+
+    const mongoose = require('mongoose');
+    const device = require('./device')
+    const logs = require('./logs')
+    
+        const schemaOptions = {
+            timestamps: true,
+            toJSON: {
+                virtuals: false
+            },
+            toObject: {
+                virtuals: false
+            }
+        }
+        
+        const testproject_collectionSchema = new mongoose.Schema(
+            {
+                version: {
+                    type: String,
+                    required: [true, 'Log version is required.']
+                },
+                type: {
+                  type: String,
+                  enum: ["001","002"],
+                  required: [true, "Atleast one model required."]
+                },
+                device:{ type: mongoose.Schema.Types.ObjectId, ref: 'Device' },
+                log:logs
+            },
+            schemaOptions
+        )
+
+        testproject_collectionSchema.index({'type': 1})
+                
+        const testproject_collection = mongoose.model('testproject_collection', testproject_collectionSchema)
+        
+        module.exports = testproject_collection
+        
