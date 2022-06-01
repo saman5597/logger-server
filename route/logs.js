ce7b72a6c9e2062207093061b520a25a4a9bbc64
Saman Arshad <samanarshad@Samans-MacBook-Air.local>
2022-06-01 13:52:42 +0530

    File name changed to original filename

--- route/logs.js
+++ route/logs.js
@@ -6,7 +6,7 @@ var maxSize = 1 * 1024 * 1024
 const storage = multer.diskStorage({
   destination: "./public/uploads/",
   filename: function (req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`);
+    cb(null, file.originalname)
   }
 });
 
